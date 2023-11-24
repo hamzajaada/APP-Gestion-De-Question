37a688697bc37c9de59840b420ef51f50be21f85
hamzajaada <hamzajaada22@gmail.com>
2023-11-25 00:18:56 +0100

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,27 @@
 const user = require("../model/User")
+const bcrypt = require('bcrypt');
 
-const addUser = (req,res)=>{  
-  console.log(req.body.password);
-  
-  const User = new user(req.body) 
-  if(req.body.password === req.body.confirmpassword){ 
-    User.save().then((result)=>{
-    console.log("Created !!!");
-    res.redirect("/Login") 
-  })
-  .catch((err)=>{
-    console.log(err);
-  })
-  }
-  else{
-    res.render("register",{ error : "le motepass est incorect"})
-  }
-   
-}
+const addUser = async (req, res) => {
+    try {
+        console.log(req.body.password);
+
+        if (req.body.password === req.body.confirmpassword) {
+            // Hasher le mot de passe
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+            // Créer un nouvel utilisateur avec le mot de passe haché
+            const User = new user({...req.body,password: hashedPassword});
+
+            // Sauvegarder l'utilisateur
+            await User.save();
+
+            console.log("Created !!!");
+            res.redirect("/Login");
+        } else {
+            res.render("register", { error: "Le mot de passe est incorrect" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erreur lors de la création de l'utilisateur");
+    }}
 module.exports = addUser;
--- midellwares/auth.js
+++ midellwares/auth.js
@@ -1,31 +1,55 @@
 
-const user = require("../model/User");
-
+const User = require("../model/User");
+const bcrypt = require('bcrypt');
 
 async function login(req, res) {
-    // try {
+    try {
         const { email, password } = req.body;
-        const data = {
-            email: email,
-            password: password,
-        };
-        const User = await user.findOne({ email: email });
+        const user = await User.findOne({ email: email });
 
-        if (User) {
-            if (data.password === User.password) {
+        if (user) {
+            const match = await bcrypt.compare(password, user.password);
+
+            if (match) {
                 console.log('Connected!');
-                req.session.user = User; // Stocker l'utilisateur dans la session
-               
-                res.render("home",{user:req.session.user});
+                req.session.user = user;
+                res.render("home", { user: req.session.user });
             } else {
                 console.log("Erreur d'authentification");
             }
         } else {
             console.log("Utilisateur non trouvé");
         }
-    // } catch (error) {
-    //     res.send(error);
-    // }
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+
+// async function login(req, res) {
+//     // try {
+//         const { email, password } = req.body;
+//         const data = {
+//             email: email,
+//             password: password,
+//         };
+//         const User = await user.findOne({ email: email });
+
+//         if (User) {
+//             if (data.password === User.password) {
+//                 console.log('Connected!');
+//                 req.session.user = User; // Stocker l'utilisateur dans la session
+               
+//                 res.render("home",{user:req.session.user});
+//             } else {
+//                 console.log("Erreur d'authentification");
+//             }
+//         } else {
+//             console.log("Utilisateur non trouvé");
+//         }
+//     // } catch (error) {
+//     //     res.send(error);
+//     // }
+// }
 
 module.exports = login;
